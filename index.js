8ca1a296a9fc7e317ae67372bd33e0094fda08c6
paulrcook <paul@lougeek.com>
2017-06-14 16:29:55 -0400

    Create WikiSearch class

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-function search(url) {
-     return $.ajax({
-        url: url,
-        method: 'GET',
-        dataType: 'jsonp',
-        success: (data) => {
-            searchResults(data.query.pages);
-        }
-    });
+const apiUrl = 'https://en.wikipedia.org/w/api.php?action=query&format=json&list=search&utf8=1&srprop=size|wordcount|timestamp|sectionsnippet&srsearch=';
+// $(document).ready(() => {
+//     $('#search').keypress((e) => {
+//         if ( event.which == 13 )
+//             wikiSearch.search(apiUrl + $('#search').val());
+//     });
+// });
+
+class WikiSearch {
+    searchInput() {
+        $('#search').keypress((e) => {
+            if ( event.which == 13 )
+                this.search(apiUrl + $('#search').val());
+        });
+    }
+
+    search(url) {
+        return $.ajax({
+            url: url,
+            method: 'GET',
+            dataType: 'jsonp',
+            success: (data) => {
+                this.searchResults(data);
+            }
+        });
+    }
+
+    searchResults(results) {
+        console.log(results);
+        $('#searchResults').append(results);
+    }
 }
-search('https://en.wikipedia.org/w/api.php?action=query&format=json&prop=revisions&titles=Main+Page&rvprop=content');
-
-
-function searchResults(results) {
-
-}
+let wikiSearch = new WikiSearch();
+$(document).ready(() => {
+    wikiSearch.searchInput();
+});
